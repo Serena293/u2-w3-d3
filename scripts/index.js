c9710305712d0ca0a95f31f7fc2573eff4ecb953
SERENA FERRARO <serenaferraro21@gmail.com>
2024-11-13 13:03:47 +0100

    library completed

---
+++ scripts/index.js
@@ -0,0 +1,47 @@
+const container = document.getElementById("container");
+const rowContainer = document.getElementById("row-container");
+
+const getBooks = () => {
+  const booksURL = "https://striveschool-api.herokuapp.com/books";
+  fetch(booksURL)
+    .then((response) => {
+      console.log("Oggetto", response);
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Errore");
+      }
+    })
+    .then((books) => {
+      console.log("secondo then");
+      books.forEach((book) => {
+        const newSlot = document.createElement("div");
+        newSlot.classList.add("col", "col-12", "col-md-6", "col-lg-3");
+        newSlot.innerHTML = `<div class="card m-2">
+  <img src="${book.img}" class="card-img-top" alt="book cover">
+  <div class="card-body">
+    <h5 class="card-title">${book.title}e</h5>
+    <p class="card-text">Category: ${book.category}</p>
+      <p class="card-text"> Price: ${book.price}€</p>
+      <p class="card-text">ID: ${book.asin}</p>
+      <div class="d-flex justify-content-around">
+    <a href="#" class="btn btn-primary delete-btn">Scarta</a>
+     <a href="#" class="btn btn-primary">Aggiungi al carrello</a>
+     </div>
+  </div>
+</div>`;
+        rowContainer.appendChild(newSlot);
+
+        const deleteBtn = newSlot.querySelector(".delete-btn");
+        deleteBtn.addEventListener("click", (e) => {
+            e.preventDefault()
+          newSlot.remove()
+        });
+      });
+    })
+    .catch((errore) => {
+      console.log("errore");
+    });
+};
+
+getBooks();
